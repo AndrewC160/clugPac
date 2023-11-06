29b86553fdded2449275fed94bf326d973dd0f19
Andrew Clugston <andrew.clugston@ucsf.edu>
2023-11-06 15:28:30 -0800

    Added plot_genes_pileup() function for large-scale genome tracks.

--- R/plot_genes.R
+++ R/plot_genes.R
@@ -8,22 +8,37 @@
 #' @param genomic_region GRange denoting the region to be illustrated.
 #' @param gr_genes GRanges object containing gene annotations. If not provided, gtf_to_genes() is used, but this can be time consuming: better to use gtf_to_genes() earlier and provide the output GR to this function directly.
 #' @param label_genes Gene names to be labeled. Defaults to NULL, in which case no genes are labeled. Supersedes text_genes.
-#' @param label_size Size of text in labels' font.
-#' @param label_seed Seed number to use for ggrepel labels (used to maintain consistent label / text positions). Defaults to NULL.
-#' @param text_genes Gene names to be labeled with text. Defaults to NULL, in which case no genes are labeled.
-#' @param text_size Size of text font.
+#' @param label_size Size of text in labels' font. Defaults to 3.
+#' @param label_seed Seed number to use for ggrepel labels (used to maintain consistent label positions). Defaults to NULL.
+#' @param focus_genes Gene names to highlight (i.e. give an alpha of 1). If given, genes not in this list have their alpha reduced to <background_alpha>. Defaults to NULL, in which case all genes are alpha 1.
+#' @param background_alpha Alpha value to use for background (non-<focus-genes>). Defaults to 0.25.
+#' @param text_genes Gene names to be labeled with text. Defaults to NULL, in which case no genes are labeled. Genes must have non-empty gene name columns.
 #' @param text_biotypes Vector of gene biotypes to be labeled with text. Defaults to "protein_coding" and "lncRNA." If "all" is listed, all biotypes will be labeled.
-#' @param text_seed Seed number to use for ggrepel text (used to maintain consistent label / text positions). Defaults to NULL.
+#' @param text_size Size of text font. Defaults to 2.
+#' @param text_bins X-axis is split into <text_bins> "bins" and labels are stacked within each. Fewer bins result in higher stacks of labels, while lower bin numbers may result in more overlaps with labels/geometry. Defaults to 10.
+#' @param text_nudge_y Distance above each text bin's highest gene annotation above which to start adding text labels in units of gene layers (one layer = one gene annotation). Defaults to 0.25.
+#' @param text_line_height Height of text labels, useful to adjust in cases where larger text sizes are used causing them to overlap. Units of layers, defaults to 0.2.
+#' @param text_nudge_x Distance to nudge labels in the negative x-direction when staggering stacked labels. Units of fractions of the X-axis, defaults to 0.005 (text is nudged 0.5% of the x-axis per stacked label).
 #'
 #' @import ggplot2
 #' @import magrittr
 #' @import GenomicRanges
 #' @import dplyr
 #' @import ggrepel
+#' @import scales
+#' @import ggsci
 #'
 #' @export
 
-plot_genes<- function(genomic_region,gr_genes=NULL,label_genes=NULL,label_size=3,label_seed=NULL,text_genes=NULL,text_size=2,text_biotypes=c("protein_coding","lncRNA"),text_seed=NULL){
+# gr_genes <- gtf_to_genes() %>% keepStandardChromosomes(pruning.mode="coarse")
+# genomic_region <- gr_genes[gr_genes$gene_name == "DLX5"] %>% resize(width=5E6,fix="center")
+# text_biotypes="all"
+# label_genes <- "DLX5"
+
+plot_genes<- function(genomic_region,gr_genes=NULL,label_genes=NULL,label_size=3,label_seed=NULL,
+                      focus_genes=NULL,background_alpha=0.25,
+                      text_genes=NULL,text_biotypes=c("protein_coding","lncRNA"),
+                      text_size=2,text_bins=10,text_nudge_y=0.25,text_line_height=0.2,text_nudge_x = 0.005){
   rename  <- dplyr::rename
   mutate  <- dplyr::mutate
   filter  <- dplyr::filter
@@ -34,81 +49,129 @@ plot_genes<- function(genomic_region,gr_genes=NULL,label_genes=NULL,label_size=3
   }
   gr <- gr_genes[queryHits(findOverlaps(gr_genes,genomic_region))]
 
-  base_plot   <-
-    ggplot(data=NULL) +
+  base_plot   <- ggplot(data=NULL) +
     scale_x_continuous(name = grange_desc(genomic_region),
                        limits=x_rng,expand=c(0,0),labels = comma) +
-    theme(panel.background = element_blank(),
+    scale_fill_igv(name="Biotype") +
+    guides(alpha="none",
+           color="none") +
+    theme(plot.background = element_rect(fill="white",color=NA),
+          plot.margin = unit(c(0,0,0,0),"lines"),
+          panel.background = element_blank(),
           panel.border = element_rect(linewidth=0.5,color="black",fill=NA),
           panel.grid.major.y = element_blank(),
-          panel.grid.major.x = element_line(linewidth=0.25,color="gray",linetype = "dotted"),
-          axis.text.x = element_text(face="italic"),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.title.y = element_blank(),
-          legend.title = element_blank())
+          panel.grid.major.x = element_line(linewidth=0.5,color="gray",linetype = "dotted"),
+          axis.text = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank())
 
   if(length(gr) > 0){
     if(is.null(label_genes))label_genes <- ""
     if(is.null(text_genes)) text_genes  <- ""
+    if(is.null(focus_genes)) focus_genes<- unique(gr$gene_name)
     if("all" %in% tolower(text_biotypes)) text_biotypes  <- unique(gr$gene_biotype)
+    bt_levs <- c("protein_coding","miRNA","lncRNA")
+    bt_levs <- c(bt_levs,setdiff(levels(gr_genes$gene_biotype),bt_levs))
     tb_p  <- gr %>%
       as_tibble %>%
-      mutate(ymin = pile_coords(start_vals = start,end_vals = end,seqname_vals = seqnames),
+      mutate(ymin = pile_coords(start_vals = start,end_vals = end,seqname_vals = seqnames,min_gap=0.001 * diff(x_rng)),
              ymax = ymin + 1,
              start_adj = ifelse(start < x_rng[1],x_rng[1],start),
              end_adj = ifelse(end > x_rng[2],x_rng[2],end),
              start = ifelse(strand == "-",end_adj,start_adj),
-             end = ifelse(strand == "-",start_adj,end_adj)) %>%
+             end = ifelse(strand == "-",start_adj,end_adj),
+             gene_biotype = factor(gene_biotype,levels=bt_levs)) %>%
       select(-start_adj,-end_adj) %>%
       mutate(start2 = start + 0.2 * (end - start),
              end2 = end - 0.2 * (end - start),
+             mid = (start + end) / 2,
+             focus_type = ifelse(gene_name %in% focus_genes,"foreground","background"),
              lab_type = case_when(gene_name == "" ~ "none",
                                   gene_name %in% label_genes ~ "label",
                                   gene_name %in% text_genes ~ "text",
                                   gene_biotype %in% text_biotypes ~ "text",
                                   TRUE ~ "none"))
-    y_rng <- c(0,max(c(6,max(tb_p$ymax))))
 
-    base_plot  <- base_plot +
-      scale_y_continuous(limits=y_rng) +
-      geom_rect(data=tb_p,
-                mapping=aes(xmin = start,xmax=end,
-                            ymin = ymin, ymax=ymax,
-                            fill = gene_biotype),
-                size=0.25,color="black",alpha=0.7) +
-      geom_segment(data=filter(tb_p,strand != "*"),
-                   mapping=aes(x=start2,xend = end2,
-                               y=(ymin + ymax)/2,yend=(ymin + ymax) / 2),
-                   size=1,color="black",
-                   arrow = arrow(length = unit(0.5,"lines"),type = "closed"))
+    tb_labs <- tb_p %>%
+      filter(lab_type != "none") %>%
+      arrange(desc(start),desc(end)) %>%
+      mutate(text_bin = cut(start,breaks=text_bins,dig.lab = 10)) %>%
+      mutate(text_bin_start = str_match(as.character(text_bin),"^[\\[\\(]([[:digit:]\\.]+)")[,2] %>% as.double %>% ceiling,
+             text_bin_end = str_match(as.character(text_bin),"([[:digit:]\\.]+)[\\]\\)]$")[,2] %>% as.double %>% ceiling) %>%
+      group_by(text_bin) %>%
+      mutate(text_y1 = max(ymax) + text_nudge_y,
+             text_y2 = text_y1 + text_line_height* row_number() * max(ymax),
+             text_bin_start = text_bin_start - (row_number() * text_nudge_x * diff(x_rng)),
+             text_bin_start = ifelse(text_bin_start < x_rng[1],x_rng[1],text_bin_start))
+
+    # Scale y up for very large windows to represent "zoomed out" scale.
+    min_y_scale <- 6 + ceiling(width(genomic_region)/1E6)/2
+    y_rng <- c(0,max(c(min_y_scale,tb_labs$text_y2,tb_p$ymax)))
+    scale_y <- scale_y_continuous(name = "Genes",limits=c(-1,y_rng[2]+1),expand=c(0,0))
 
     #Gene text.
-    tb_labs <- filter(tb_p,lab_type == "text")
-    if(nrow(tb_labs) > 0){
+    tb_l <- filter(tb_labs,lab_type == "text")
+    if(nrow(tb_l) > 0){
       base_plot <- base_plot +
-        geom_text_repel(data=tb_labs,
-                  seed = text_seed,
-                  size=text_size,
-                  mapping=aes(x=(start + end) / 2,
-                              y=(ymin + ymax) / 2,
-                              label = gene_name),
-                  min.segment.length = 0.01,
-                  nudge_y = sample(c(1,-1),replace=TRUE))#,size = nrow(tb_labs)))
+        geom_segment(data=tb_l,
+                     mapping=aes(x=text_bin_start,
+                                 xend=text_bin_start,
+                                 y=text_y2,yend=text_y1,
+                                 alpha=focus_type),
+                     color="gray75") +
+        geom_segment(data=tb_l,
+                     mapping=aes(x=text_bin_start,
+                                 xend=start,
+                                 y=text_y1,yend=ymax,
+                                 alpha=focus_type),
+                     color="gray75") +
+        geom_segment(data=tb_l,
+                     mapping=aes(x=start,
+                                 xend=start,
+                                 y=ymax,yend=(ymin+ymax)/2,
+                                 alpha=focus_type),
+                     color="gray75") +
+        geom_text(data=tb_l,
+                  mapping=aes(x=text_bin_start,
+                              y=text_y2,
+                              label=gene_name,
+                              alpha=focus_type),
+                  hjust=0,vjust=0,
+                  size=text_size)
     }
+
+    base_plot  <- base_plot +
+      scale_alpha_manual(values=c(foreground=1,background=background_alpha)) +
+      scale_color_manual(values=c(foreground="black",background=NA)) +
+      scale_y +
+      geom_rect(data=tb_p,
+                mapping=aes(xmin = start,xmax=end,
+                            ymin = ymin+0.1, ymax=ymax-0.1,
+                            fill = gene_biotype,
+                            alpha=focus_type,
+                            color=focus_type),
+                linewidth=0.25) +
+      geom_segment(data=filter(tb_p,strand != "*" & focus_type != "background" & lab_type != "none"),
+                   mapping=aes(x=start2,xend = end2,
+                               y=(ymin + ymax)/2,yend=(ymin + ymax) / 2,
+                               alpha=focus_type,
+                               color=focus_type),
+                   linewidth=1,
+                   arrow = arrow(length = unit(0.5,"lines"),type = "closed"))
+
     #Gene labels.
-    tb_labs <- filter(tb_p,lab_type == "label")
-    if(nrow(tb_labs) > 0){
+    tb_l <- filter(tb_labs,lab_type == "label")
+    if(nrow(tb_l) > 0){
       base_plot <- base_plot +
         geom_label_repel(
-          data=tb_labs,
+          data=tb_l,
           seed = label_seed,
           mapping=aes(x=(start + end) / 2,
-                      y=(ymin + ymax) / 2,
-                      label = gene_name),
+                      y=ymin+0.1,
+                      label = gene_name,
+                      alpha=focus_type),
           min.segment.length = 0.01,
-          size=label_size,
-          nudge_y = 2)
+          size=label_size,ylim = c(0,-0.5))
     }
   }
   return(base_plot)
---
+++ R/plot_genes_pileup.R
@@ -0,0 +1,80 @@
+#' @title
+#' Plot gene track pileup.
+#'
+#' @description
+#' Given a genomic range, breaks the range into <bin_num> bins and plots the
+#' number of genes per bin. Specific genes can be labeled using <label_genes>.
+#'
+#' @param genomic_region GRange denoting the region to be illustrated.
+#' @param gr_genes_in GRanges object containing gene annotations. If not provided, gtf_to_genes() is used, but this can be time consuming: better to use gtf_to_genes() earlier and provide the output GR to this function directly.
+#' @param bin_num Number of bins to split the range into. Defaults to 300.
+#' @param label_genes Gene names to be labeled. Defaults to NULL, in which case no genes are labeled. Supersedes text_genes.
+#' @param label_size Size of text in labels' font. Defaults to 3.
+#' @param label_seed Seed number to use for ggrepel labels (used to maintain consistent label positions). Defaults to NULL.
+#'
+#' @import ggplot2
+#' @import magrittr
+#' @import GenomicRanges
+#' @import dplyr
+#' @import ggrepel
+#' @import scales
+#'
+#' @export
+
+plot_genes_pileup <- function(genomic_region,gr_genes_in=NULL,bin_num=300,label_genes=NULL,label_size=3,label_seed=NULL,kary_frac=0.1){
+  rename  <- dplyr::rename
+  mutate  <- dplyr::mutate
+  filter  <- dplyr::filter
+  x_rng   <- c(start=GenomicRanges::start(genomic_region),end=GenomicRanges::end(genomic_region))
+  if(is.null(gr_genes_in)){
+    message("Gathering genes from GFF file via gtf_to_genes(), provide a GR object to 'gr_genes_in' in order to speed this up in the future.")
+    gr_genes_in <- gtf_to_genes() %>% keepStandardChromosomes(pruning.mode="coarse")
+  }
+  gr_gns <- subsetByOverlaps(gr_genes_in,genomic_region)
+
+  gr_bins <- tile(genomic_region,n = bin_num) %>% unlist
+  gr_bins$bin_id  <- paste0("bin_",c(1:length(gr_bins)))
+  gr_bins$count <- countOverlaps(gr_bins,gr_genes_in)
+
+  tb_p <- as_tibble(gr_bins) %>%
+    mutate(pMb = count / (width/1e6))
+  bin_size <- prettyBP(mean(width(gr_bins)),digits=0)
+
+  y_max <- max(tb_p$count)
+  y_rng <- NULL
+  geom_labs <- NULL
+  if(!is.null(label_genes)){
+    tb_labs <- gr_gns[gr_gns$gene_name %in% label_genes] %>% as_tibble
+    if(nrow(tb_labs) > 0){
+      y_rng <- c(-y_max*0.3,y_max*1.1)
+      geom_labs <- geom_label_repel(data=tb_labs,mapping=aes(y=0,x=(start+end)/2,label=gene_name),
+                                    size=label_size,nudge_y = -0.1 * y_max,inherit.aes=FALSE,
+                                    min.segment.length = 0)
+    }
+  }
+  if(is.null(y_rng)){
+    y_rng <- c(-y_max * 0.1,y_max*1.1)
+  }
+
+  tb_kary <- get_karyotypes() %>%
+    subsetByOverlaps(genomic_region) %>%
+    as_tibble %>%
+    mutate(ymax=0,ymin=y_max*kary_frac)
+
+  ggplot(tb_p,aes(xmin=start,xmax=end,ymin=0,ymax=count)) +
+    scale_x_continuous(name=grange_desc(genomic_region,append_ending = paste0("; ",bin_size," bin size")),
+                       expand=c(0,0),limits=x_rng,labels=prettyBP) +
+    scale_y_continuous(name="Genes per bin",expand=c(0,0),limits=y_rng) +
+    geom_rect(fill="gray40",color="gray20") +
+    geom_rect(data=tb_kary,mapping=aes(xmin=start,xmax=end,ymin=ymin,ymax=ymax,fill=fill),
+              inherit.aes=FALSE) +
+    annotate(geom="rect",xmin=x_rng[1],xmax=x_rng[2],ymax=0,ymin=kary_frac*y_max,fill=NA,color="black",linewidth=0.5) +
+    scale_fill_identity() +
+    geom_labs +
+    theme(plot.background = element_rect(fill="white",color=NA),
+          plot.margin = unit(c(0,0,0,0),"lines"),
+          panel.background = element_blank(),
+          panel.border = element_rect(linewidth=0.5,color="black",fill=NA),
+          panel.grid.major.y = element_blank(),
+          panel.grid.major.x = element_line(linewidth=0.5,color="gray",linetype = "dotted"))
+}
