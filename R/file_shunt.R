a7d7dd051b67010878a7d2b9056524772e1c1396
Andrew Clugston <andrew.clugston@ucsf.edu>
2025-01-14 13:24:46 -0800

    Added overwrite/recursive functionality to file_shunt().

--- R/file_shunt.R
+++ R/file_shunt.R
@@ -8,13 +8,23 @@
 #'
 #' @param fileA Origin file.
 #' @param fileB Destination file.
+#' @param create_dir Boolean; should the output directory be created if it is missing? Defaults to FALSE.
+#' @param over_write Boolean; should an existing fileB be over-written? Defaults to FALSE.
 #'
 #' @export
 
-file_shunt <- function(fileA,fileB){
+file_shunt <- function(fileA,fileB,create_dir=FALSE,over_write=FALSE){
   if(!file.exists(fileA)) stop("File ",fileA,"not found.")
-  if(!file.exists(fileB)){
-    file.copy(fileA,fileB)
+  if(!file.exists(fileB) | over_write){
+    out_dir <- dirname(fileB)
+    if(!dir.exists(out_dir)){
+      if(!create_dir){
+        stop("Output directory not found, create it manually or via 'create_dir=TRUE'.")
+      }else{
+        dir.create(out_dir,recursive = TRUE)
+      }
+    }
+    file.copy(from = fileA,to = fileB,overwrite = TRUE)
   }
   return(fileB)
 }
